b6330fac1dd9ca867268bb662aec7b4e800a1cba
gzfgeh <gzfgeh@qq.com>
2015-03-14 23:01:55 +0800

    fragment

--- Fragment/src/com/gzfgeh/fragment/MainActivity.java
+++ Fragment/src/com/gzfgeh/fragment/MainActivity.java
@@ -1,10 +1,6 @@
 package com.gzfgeh.fragment;
 
-
-import com.gzfgeh.login.SignFragment;
-
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.app.AlertDialog;
@@ -13,88 +9,26 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.os.Build;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-	private SignFragment sign;
-	private PlaceholderFragment placeholderFragment;
-	
-	private EditText userName,passWord;
-	private EditText signUserName,signPassWord;
-	private Button loadButton, signButton, nextSignButton;
-	private String userNameString, passWordString;
-	private String signUserNameString,signPassWordString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        userName = (EditText) findViewById(R.id.login_user_input);
-        passWord = (EditText) findViewById(R.id.login_password_input);
-        signUserName = (EditText) findViewById(R.id.sign_user_input);
-        signPassWord = (EditText) findViewById(R.id.sign_password_input);
-        
-        loadButton = (Button) findViewById(R.id.login_button);
-        signButton = (Button) findViewById(R.id.signin_button);
-        nextSignButton = (Button) findViewById(R.id.next_signin_button);
         
         if (savedInstanceState == null) {
-        	placeholderFragment = new PlaceholderFragment();
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, placeholderFragment)
+                    .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
         
-    }
-    
-    public void onLoaderClick (View view){
-    	
-    	FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-    	
-    	switch (view.getId()) {
-		case R.id.login_button:
-			userNameString = userName.getText().toString().trim();
-	    	passWordString = passWord.getText().toString().trim();
-	    	
-			if (userNameString == null || passWordString == null){
-				new AlertDialog.Builder(this).
-				setMessage("账号或者密码不能为空").show();
-			}else{
-				
-			}
-				
-			break;
-			
-		case R.id.signin_button:
-			if (sign == null){
-				sign = new SignFragment();
-				ft.add(sign, "sign");
-			}
-			ft.hide(placeholderFragment);
-			ft.addToBackStack(null);
-			ft.commit();
-			break;
-			
-		case R.id.next_signin_button:
-			signUserNameString = signUserName.getText().toString().trim();
-	    	signPassWordString = signPassWord.getText().toString().trim();
-	    	
-	    	if (signUserNameString == null || signPassWordString == null){
-				new AlertDialog.Builder(this).
-				setMessage("账号或者密码不能为空").show();
-			}else{
-				
-			}
-			break;
-			
-		default:
-			break;
-		}
     }
     
     @Override
@@ -121,18 +55,80 @@ public class MainActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-    	
-    	
-        public PlaceholderFragment() {
+    	private Button loadButton, signButton;
+    	private EditText userName,passWord;
+    	private String userNameString, passWordString;
+
+		public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            
             return rootView;
         }
+
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onActivityCreated(savedInstanceState);
+            loadButton = (Button) getActivity().findViewById(R.id.login_button);
+            signButton = (Button) getActivity().findViewById(R.id.signin_button);
+            userName = (EditText) getActivity().findViewById(R.id.username);
+            passWord = (EditText) getActivity().findViewById(R.id.password);
+            loadButton.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View view) {
+					// TODO Auto-generated method stub
+					userNameString = userName.getText().toString().trim();
+	    	    	passWordString = passWord.getText().toString().trim();
+	    	    	
+	    			if (userNameString == " " || userNameString == null 
+	    					|| passWordString == " " || passWordString == null){
+	    				AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	    				builder.setMessage(R.string.login_warning);
+	    				builder.show();
+	    			}else{
+	    				
+	    			}
+				}
+			});
+		}
+        
+        
+//        public void onLoaderClick (View view){
+//        	FragmentTransaction ft = getFragmentManager().beginTransaction();
+//        	switch (view.getId()) {
+//    		case R.id.login_button:
+//    			userNameString = userName.getText().toString().trim();
+//    	    	passWordString = passWord.getText().toString().trim();
+//    	    	
+//    			if (userNameString == null || passWordString == null){
+//    				AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//    				builder.setMessage(R.string.login_warning);
+//    				builder.show();
+//    			}else{
+//    				
+//    			}
+//    				
+//    			break;
+//    			
+//    		case R.id.signin_button:
+////    			if (sign == null){
+////    				sign = new SignFragment();
+////    				ft.add(sign, "sign");
+////    			}
+//    			ft.hide(this);
+//    			ft.addToBackStack(null);
+//    			ft.commit();
+//    			break;
+//    			
+//    		default:
+//    			break;
+//    		}
+//        }
         
     }
 
--- Fragment/src/com/gzfgeh/login/SignFragment.java
+++ Fragment/src/com/gzfgeh/login/SignFragment.java
@@ -10,37 +10,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignFragment extends Fragment {
+	private Button nextSignButton;
+	private EditText signUserName,signPassWord;
+	private String signUserNameString,signPassWordString;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.signin, container,false);
+		signUserName = (EditText) view.findViewById(R.id.sign_user_input);
+        signPassWord = (EditText) view.findViewById(R.id.sign_password_input);
+        nextSignButton = (Button) view.findViewById(R.id.next_signin_button);
 		return view;
 	}
 	
-//	public void signOnLoaderClick(View view){
-//		userNameString = userName.getText().toString();
-//		passWordString = passWord.getText().toString();
-//		
-//		if (userNameString == null || passWordString == null)
-//			Toast.makeText(getActivity(), "不能为空", Toast.LENGTH_SHORT).show();
-//		else {
-//			SharedPreferences sharedPreferences = getActivity().getSharedPreferences("password", Context.MODE_PRIVATE);
-//			Editor editor = sharedPreferences.edit();
-//			editor.putString("name", userNameString);
-//			editor.putString("password", passWordString);
-//			editor.commit();
-//			
-//			
-//			Bundle bundle = new Bundle();
-//			bundle.putString("Status", "OK");
-//			this.setArguments(bundle);
-//			getFragmentManager().beginTransaction().hide(this).commit();
-//		}
-//	}
+	public void signOnLoaderClick(View view){
+		signUserNameString = signUserName.getText().toString();
+		signPassWordString = signPassWord.getText().toString();
+		
+		if (signUserNameString == null || signPassWordString == null)
+			Toast.makeText(getActivity(), "不能为空", Toast.LENGTH_SHORT).show();
+		else {
+			SharedPreferences sharedPreferences = getActivity().getSharedPreferences("password", Context.MODE_PRIVATE);
+			Editor editor = sharedPreferences.edit();
+			editor.putString("name", signUserNameString);
+			editor.putString("password", signPassWordString);
+			editor.commit();
+			
+			
+			Bundle bundle = new Bundle();
+			bundle.putString("Status", "OK");
+			this.setArguments(bundle);
+			getFragmentManager().beginTransaction().hide(this).commit();
+		}
+	}
 }
