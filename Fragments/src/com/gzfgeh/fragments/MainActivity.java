a78aa5f923ca850b921a80b23304366abe1b5941
gzfgeh <gzfgeh@qq.com>
2015-03-16 00:27:32 +0800

    add viewPage and FragmentViewPage

--- Fragments/src/com/gzfgeh/fragments/MainActivity.java
+++ Fragments/src/com/gzfgeh/fragments/MainActivity.java
@@ -1,7 +1,14 @@
 package com.gzfgeh.fragments;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.ActionBarActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -13,14 +20,19 @@ import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity implements OnClickListener {
+public class MainActivity extends ActionBarActivity implements OnClickListener, OnPageChangeListener {
 	private View textLayout, soundsLayout, photoLayout, movieLayout;
 	private TextView textView, soundsView, photoView, movieView;
+	private TextView titleText;
 	private TextFragment textFragment;
 	private SoundsFragment soundsFragment;
 	private PhotoFragment photoFragment;
 	private MovieFragment movieFragment;
 	private FragmentManager fragmentManager;
+	private ViewPager viewPager;
+	private FragmentPagerAdapter fragmentPagerAdapter;
+	private List<Fragment> fragments = new ArrayList<Fragment>();
+	private int currentSelect = -1;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +43,26 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         if (savedInstanceState == null) {
             initViews();
             fragmentManager = getSupportFragmentManager();
-            selectFragmentNum(0);
+            fragmentPagerAdapter = new FragmentPagerAdapter(fragmentManager) {
+				
+				@Override
+				public int getCount() {
+					// TODO Auto-generated method stub
+					return fragments.size();
+				}
+				
+				@Override
+				public Fragment getItem(int position) {
+					// TODO Auto-generated method stub
+					return fragments.get(position);
+				}
+			};
+            
+			viewPager.setAdapter(fragmentPagerAdapter);
+			
+			viewPager.setOnPageChangeListener(this);
+			
+            //selectFragmentNum(0);
         }
     }
 
@@ -39,47 +70,34 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
     @SuppressLint("ResourceAsColor") private void selectFragmentNum(int i) {
 		// TODO Auto-generated method stub
 		FragmentTransaction ft = fragmentManager.beginTransaction();
-		hideAllFragments(ft);
-		
+		//hideAllFragments(ft);
+		resetSelected();
+		currentSelect = i;
 		switch (i) {
 		case 0:
+			titleText.setText(R.string.text);
 			textView.setTextColor(getResources().getColor(R.color.title_bg));
-			if (textFragment == null){
-				textFragment = new TextFragment();
-				ft.add(R.id.container, textFragment);
-			}else{
-				ft.show(textFragment);
-			}
+			ft.show(textFragment);
 			break;
 			
 		case 1:
+			titleText.setText(R.string.photo);
 			photoView.setTextColor(getResources().getColor(R.color.title_bg));
-			if (photoFragment == null){
-				photoFragment = new PhotoFragment();
-				ft.add(R.id.container, photoFragment);
-			}else{
-				ft.show(photoFragment);
-			}
+			ft.show(photoFragment);
 			break;
 			
 		case 2:
+			titleText.setText(R.string.sounds);
 			soundsView.setTextColor(getResources().getColor(R.color.title_bg));
-			if (soundsFragment == null){
-				soundsFragment = new SoundsFragment();
-				ft.add(R.id.container, soundsFragment);
-			}else{
-				ft.show(soundsFragment);
-			}
+			ft.show(soundsFragment);
 			break;
+			
 		case 3:
+			titleText.setText(R.string.movie);
 			movieView.setTextColor(getResources().getColor(R.color.title_bg));;
-			if (movieFragment == null){
-				movieFragment = new MovieFragment();
-				ft.add(R.id.container, movieFragment);
-			}else{
-				ft.show(movieFragment);
-			}
+			ft.show(movieFragment);
 			break;
+			
 		default:
 			break;
 		}
@@ -87,7 +105,7 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 	}
 
 
-	@SuppressLint("ResourceAsColor") private void hideAllFragments(FragmentTransaction ft) {
+	private void hideAllFragments(FragmentTransaction ft) {
 		// TODO Auto-generated method stub
 		if (textFragment != null)
 			ft.hide(textFragment);
@@ -100,13 +118,14 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		
 		if (movieFragment != null)
 			ft.hide(movieFragment);
-		
+	}
+	
+	@SuppressLint("ResourceAsColor") private void resetSelected(){
 		textView.setTextColor(R.color.bottom_bg);
 		soundsView.setTextColor(R.color.bottom_bg);
 		photoView.setTextColor(R.color.bottom_bg);
 		movieView.setTextColor(R.color.bottom_bg);
 	}
-
 
 	private void initViews() {
 		// TODO Auto-generated method stub
@@ -122,6 +141,21 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		soundsView = (TextView) findViewById(R.id.sounds_msg);
 		photoView = (TextView) findViewById(R.id.photo_msg);
 		movieView = (TextView) findViewById(R.id.movie_msg);
+		titleText = (TextView) findViewById(R.id.title_text);
+		
+		viewPager = (ViewPager) findViewById(R.id.container);
+		textFragment = new TextFragment();
+		soundsFragment = new SoundsFragment();
+		photoFragment = new PhotoFragment();
+		movieFragment = new MovieFragment();
+		fragments.add(textFragment);
+		fragments.add(photoFragment);
+		fragments.add(soundsFragment);
+		fragments.add(movieFragment);
+		
+		titleText.setText(R.string.text);
+		textView.setTextColor(getResources().getColor(R.color.title_bg));
+		
 	}
 
 
@@ -150,23 +184,67 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		// TODO Auto-generated method stub
 		switch (view.getId()) {
 		case R.id.text_layout:
-			selectFragmentNum(0);
+			onPageSelected(0);
 			break;
 
 		case R.id.photo_layout:
-			selectFragmentNum(1);
+			onPageSelected(1);
 			break;
 			
 		case R.id.sounds_layout:
-			selectFragmentNum(2);
+			onPageSelected(2);
 			break;
 			
 		case R.id.movie_layout:
-			selectFragmentNum(3);
+			onPageSelected(3);
 			break;
 			
 		default:
 			break;
 		}
+	}
+
+
+	@Override
+	public void onPageScrollStateChanged(int position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onPageSelected(int position) {
+		// TODO Auto-generated method stub
+		resetSelected();
+		
+		switch (position) {
+		case 0:
+			titleText.setText(R.string.text);
+			textView.setTextColor(getResources().getColor(R.color.title_bg));
+			break;
+			
+		case 1:
+			titleText.setText(R.string.photo);
+			photoView.setTextColor(getResources().getColor(R.color.title_bg));
+			break;
+			
+		case 2:
+			titleText.setText(R.string.sounds);
+			soundsView.setTextColor(getResources().getColor(R.color.title_bg));
+			break;
+		case 3:
+			titleText.setText(R.string.movie);
+			movieView.setTextColor(getResources().getColor(R.color.title_bg));
+			break;
+		}
+		
+		currentSelect = position;
 	}
 }
